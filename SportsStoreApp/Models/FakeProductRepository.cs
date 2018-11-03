6fdeedb235187cbf106dedf78d6ad91b3ff0e48f
Andrei Pugatsov <andrei.pugatsov@gmail.com>
2018-11-03 15:36:29 +0200

    EF DB created and seeded

---
+++ SportsStoreApp/Models/FakeProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreApp.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product {Name = "pr1", Price = 5},
+            new Product {Name = "pr2", Price = 14},
+            new Product {Name = "pr3", Price = 25},
+            new Product {Name = "pr4", Price = 55},
+        }.AsQueryable<Product>();
+    }
+}
